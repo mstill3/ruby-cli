691c6eadc2956a9c7b67d2cb7e3c64b4d3aa6ccb
Matt Stillwell <stillwell006@gmail.com>
2024-07-21 15:12:53 -0400

    :zap: Improve code

--- main.rb
+++ main.rb
@@ -5,12 +5,12 @@ require 'git'
 
 puts 'hello world'
 
-CLI::UI::Prompt.ask('What language/framework do you use?') do |handler|
-  handler.option('rails')  { |selection| selection }
-  handler.option('go')     { |selection| selection }
-  handler.option('ruby')   { |selection| selection }
-  handler.option('python') { |selection| selection }
-end
+# CLI::UI::Prompt.ask('What language/framework do you use?') do |handler|
+#   handler.option('rails')  { |selection| selection }
+#   handler.option('go')     { |selection| selection }
+#   handler.option('ruby')   { |selection| selection }
+#   handler.option('python') { |selection| selection }
+# end
 
 working_dir = '.'
 git = Git.open(working_dir)
@@ -20,10 +20,17 @@ git = Git.open(working_dir)
 #g.index.writable?
 #g.repo
 
-git.log.each {|l| puts l.sha }
+# git.log.each {|l| puts l.sha }
   #.max_count(:all)
   #.object('README.md')
   #.since('10 years ago')
   #.between('v1.0.7', 'HEAD')
   #.map { |commit| commit.sha }
+
+puts git.branch
+
+git.branches.each { |branch|
+  puts branch
+  puts ''
+}
 
